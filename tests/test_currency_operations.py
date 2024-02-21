0d8d39868a1dbf8b7fefc763002581d0bb57b788
Artem Khoroshilov <artemhorik11@gmail.com>
2024-02-21 22:04:58 +0200

    Test for convert currency func added

---
+++ tests/test_currency_operations.py
@@ -0,0 +1,16 @@
+import pytest
+from unittest.mock import patch
+
+from app.db.currency_operations import convert_currency
+
+
+@pytest.mark.asyncio
+@patch('app.db.currency_operations.get_currency_rate')
+async def test_convert_currency(mock_get_currency_rate):
+    mock_get_currency_rate.side_effect = [1.2,
+                                          0.8]
+
+    converted_amount = await convert_currency(None, 'USD', 'EUR', 100)
+
+    expected_amount = 100 * (0.8 / 1.2)
+    assert converted_amount == expected_amount, "The converted amount does not match the expected value."
