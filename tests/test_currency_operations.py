4116d79b0ce65f7286f7ac5c17a5dcf60aa5a703
Artem Khoroshilov <artemhorik11@gmail.com>
2024-02-22 15:28:52 +0200

    Float to Decimal bug fix

--- tests/test_currency_operations.py
+++ tests/test_currency_operations.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 import pytest
 from unittest.mock import patch
 
@@ -7,10 +9,10 @@ from app.db.currency_operations import convert_currency
 @pytest.mark.asyncio
 @patch('app.db.currency_operations.get_currency_rate')
 async def test_convert_currency(mock_get_currency_rate):
-    mock_get_currency_rate.side_effect = [1.2,
-                                          0.8]
+    mock_get_currency_rate.side_effect = [Decimal('1.2'),
+                                          Decimal('0.8')]
 
     converted_amount = await convert_currency(None, 'USD', 'EUR', 100)
 
-    expected_amount = 100 * (0.8 / 1.2)
+    expected_amount = Decimal('100') * (Decimal('0.8') / Decimal('1.2'))
     assert converted_amount == expected_amount, "The converted amount does not match the expected value."
