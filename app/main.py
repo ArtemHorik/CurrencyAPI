fb1ff0462501393d0e8faaa66d19ee04726be08e
Artem Khoroshilov <artemhorik11@gmail.com>
2024-02-23 13:30:44 +0200

    Binding to all interfaces removed

--- app/main.py
+++ app/main.py
@@ -101,4 +101,4 @@ async def convert_endpoint(source: str, target: str, amount: float, session: Asy
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
